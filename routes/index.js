b29b76f052fd50ed60b02f8e5cd3d1be7f01d5f1
Rhee <hhrhee@163.com>
2023-05-30 14:15:19 +0800

    feat: download

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,9 @@ const router = require('koa-router')();
 // const MongoID = require('mongodb').ObjectID;
 
 // const newData = require('../utils/sendNewData');
-
+// const { Readable } = require('stream');
+const fs = require('fs')
+const path = require('path')
 
 
 // const db = mongoDB.Init();
@@ -29,6 +31,21 @@ router.get('/test/post', async (ctx, next) => {
 router.get('/test/put', async (ctx, next) => {
   const post = ctx.request.body;
   ctx.body = post
+})
+
+// 创建无限下载的文件地址
+router.get('/test/download', async (ctx, next) => {
+  await new Promise((res, rej) => {
+    setTimeout(() => {
+      res()
+    }, 10000)
+  })
+  // const fileStream = fs.createReadStream(path.resolve(__dirname, '../static/test.zip'));
+  // ctx.type = 'application/zip';
+  // ctx.attachment('test.zip');
+  // ctx.body = fileStream;
+  ctx.body = 'Hello World'
+
 })
 
 module.exports = router
